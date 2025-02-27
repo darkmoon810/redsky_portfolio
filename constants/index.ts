32d2fb80e351a1b2398071f8b0c0451838819596
darkmoon810 <dark810moon@gmail.com>
2025-02-27 18:31:21 -0500

    This is my Portfolio

---
+++ constants/index.ts
@@ -0,0 +1,361 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://instagram.com",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://facebook.com",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://twitter.com",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "AI-powered chat for your product documentation.",
+    description:
+      'AI-powered chat integrates a chatbot or conversational agent into product documentation. This chatbot can provide real-time, on-demand answers, assist in navigating the documentation, and even offer personalized advice based on user queries.  Disclosure of personal information is prohibited.!!!',
+    image: "/projects/project-1.png",
+    link: "https://usecontext.io",
+  },
+  {
+    title: " Nothing But LEDs",
+    description:
+      'The website Nothing But LEDs specializes in providing high-quality LED lighting solutions for both indoor and outdoor applications. They offer a wide range of products, including LED bulbs, downlights, flood lights, high bay lights, and various accessories. The site features categories for different lighting needs such as residential, commercial, and signage lighting. They also offer weekly deals and promotions.  Disclosure of personal information is prohibited.!!!',
+    image: "/projects/project-2.png",
+    link: "https://nothingbutleds.com",
+  },
+  {
+    title: "Jewelrymine USA ",
+    description:
+      'Jewelrymine USA is an online jewelry store offering a wide selection of jewelry products, including chains, necklaces, bracelets, rings, earrings, and more. The site promotes discounts, such as a 10% off coupon and special sales.  Disclosure of personal information is prohibited.!!!',
+    image: "/projects/project-3.png",
+    link: "https://jewelrymineusa.com",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://youtube.com",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.com",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://instagram.com",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://twitter.com",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://linkedin.com",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+      {
+        name: "Become Sponsor",
+        icon: null,
+        link: "https://youtube.com",
+      },
+      {
+        name: "Learning about me",
+        icon: null,
+        link: "https://example.com",
+      },
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "mailto:contact@example.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
